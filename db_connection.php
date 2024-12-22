ad0fc6e2b87e1c1464b97a178b720cb2cef70cef
Salih Usanmaz <75589279+oku-20227170039@users.noreply.github.com>
2024-12-22 06:05:08 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,15 +1,19 @@
 <?php
 // Veritabanı bağlantı bilgileri
-$servername = "localhost"; // XAMPP kullanıyorsanız genelde localhost olur
-$username = "root"; // Varsayılan kullanıcı adı
-$password = ""; // Varsayılan şifre (boş bırakın)
-$dbname = "slh_blog"; // Kullanılacak veritabanı adı
+$servername = "autorack.proxy.rlwy.net"; // Railway bağlantı adresi (host)
+$username = "root"; // Railway kullanıcı adı
+$password = "gQHifJeubmZvpXHjpsyjmeUlfVyXNfJz"; // Railway şifresi
+$dbname = "railway"; // Railway veritabanı adı
+$port = 21459; // Railway port numarası
 
 // Bağlantıyı oluştur
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Bağlantıyı kontrol et
 if ($conn->connect_error) {
     die("Bağlantı başarısız: " . $conn->connect_error);
 }
+
+// Eğer buraya kadar hata yoksa:
+echo "Railway veritabanına başarıyla bağlanıldı!";
 ?>
